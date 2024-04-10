41fca4a3da6f748ea283bb9f7c271fa924b40a04
Filip Tomaszewski <filip.tomaszewski@protonmail.com>
2024-04-10 20:07:52 +0200

    Add algorithm to addEven function

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -2,7 +2,11 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    int total{};
+    for (const auto& number : numbers) {
+        if (number % 2 == 0) {
+            total += number;
+        }
+    }
+    return total;
 }
